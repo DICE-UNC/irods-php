5f471a5ac1e28c08563ba3a6e854ccef1a56b136
Lisa Stillwell <lisa@renci.org>
2011-08-09 14:52:53 -0400

    Added new functionality to exec a rule that returns a ExecCmdOut message type. This is a new test for that feature.

---
+++ prods/test/execCmdOutUserRule.php
@@ -0,0 +1,35 @@
+<?php
+
+require_once("../src/Prods.inc.php");
+
+$account=new RODSAccount("localhost", 1247, "rods", "RODS", "tempZone");
+
+$rule_body  = "printHello||print_hello|nop";
+$input_params = array();
+$out_params = array('ruleExecOut');
+
+try {
+$start_time=microtime(true);
+
+$conn=new RODSConn($account);
+$conn->connect();
+$results=$conn->execUserRule($rule_body,$input_params,$out_params);
+var_dump($results);
+$conn->disconnect();
+
+$rule=new ProdsRule($account,$rule_body,$input_params,$out_params);
+$results=$rule->execute(); 
+var_dump($results);
+
+$end_time=microtime(true);
+$exec_time=$end_time-$start_time;
+echo "--- test successful!  in ($exec_time sec) --- <br/>\n";
+
+} catch (RODSException $e) {
+  
+  echo "--- test failed! --- <br/>\n";
+  echo ($e);
+  echo $e->showStackTrace();
+}
+
+?>
