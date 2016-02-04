1329627c034f265361cbbcf6839d4bf224e554df
root <root@postfix.netlab.uky.edu>
2016-02-04 13:34:07 -0500

    The source registers the stream protocols as "rods" and "rods+ticket" where as the example used "irods". SO chnaged the example to use "rods"

--- prods/tutorials/Prods/streamer.php
+++ prods/tutorials/Prods/streamer.php
@@ -5,10 +5,10 @@ require_once("/Path/to/Prods/src/ProdsStreamer.class.php");
 
 // read and print the file "/tempZone/home/demouser/test.txt". assuming:
 // username: demouser, password: demopass, server: srbbrick15.sdsc.edu, port: 1247
-$str = file_get_contents("irods://demouser:demopass@srbbrick15.sdsc.edu:1247/tempZone/home/demouser/test.txt");
+$str = file_get_contents("rods://demouser:demopass@srbbrick15.sdsc.edu:1247/tempZone/home/demouser/test.txt");
 var_dump($str);
 
 // print the children of home dir "/tempZone/home/demouser"
-$children = scandir("irods://demouser:demopass@srbbrick15.sdsc.edu:1247/tempZone/home/demouser");
+$children = scandir("rods://demouser:demopass@srbbrick15.sdsc.edu:1247/tempZone/home/demouser");
 var_dump($children);
 ?>
