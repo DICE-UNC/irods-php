3211ea59dc264def251ee0bc3970cfa42a6b2d1e
michael-conway <michael_conway@unc.edu>
2016-06-22 13:10:31 -0400

    #15 adding test framework

---
+++ PHPUnitTest/prods/src/IniParser.php
@@ -0,0 +1,53 @@
+<?php
+
+require_once dirname(__FILE__) . '/../../../prods/src/RODSAccount.class.php';
+
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: mconway
+ * Date: 6/22/16
+ * Time: 9:49 AM
+ */
+class IniParser
+{
+
+
+
+    public $defaultIni = "/etc/irods-ext/phptest.ini";
+    public $ini_array;
+
+
+    /**
+     * IniParser constructor.
+     */
+    public function __construct()
+    {
+        $this->getIniData();
+    }
+
+
+    /**
+     * return a properties array based on the expected /etc/irods-ext/phptest.ini file
+     */
+    private function getIniData()
+    {
+        $this->ini_array = parse_ini_file($this->defaultIni);
+        print_r($this->ini_array);
+        return $this->ini_array;
+    }
+
+    /**
+     * Get the account for connection based on the init parameters for the
+     */
+    public function buildRodsAccountForTest1User()
+    {
+
+        $rodsAccount = new RODSAccount($this->ini_array["host"], $this->ini_array["port"], $this->ini_array["user1"],
+            $this->ini_array["user1"], $this->ini_array["zone"], $this->ini_array["def-resc"]);
+        print_r($rodsAccount);
+        return $rodsAccount;
+
+    }
+
+}
